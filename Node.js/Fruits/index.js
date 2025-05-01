b58ce7628f47ebc44b4965add74671a8e769d8ae
Manoj Dhage <manojdhage1341@gmail.com>
2025-05-01 17:33:33 +0530

    Export One directory to other

---
+++ Node.js/Fruits/index.js
@@ -0,0 +1,7 @@
+let mango=require("./mango");
+let banana=require("./banana");
+let orange=require("./orange");
+
+let fruits=[mango,banana,orange];
+
+module.exports=fruits;
--- Node.js/Script.js
+++ Node.js/Script.js
@@ -4,3 +4,14 @@ for(let i=1;i<=5;i++){
 console.log("Byeeee");
 
 //Testing file using node through terminal or gitbash.........
+
+
+
+
+
+
+///////////////accessing fruits directory using module.exports
+
+const fruits=require("./Fruits");
+
+console.log(fruits[0].color);
