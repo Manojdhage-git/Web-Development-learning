1e6ed4191daa16f0cd9fe47dd01c9a258a05a3b8
Manoj Dhage <manojdhage1341@gmail.com>
2025-05-01 17:18:52 +0530

    Module.exports

---
+++ Node.js/math.js
@@ -0,0 +1,25 @@
+// Different methods to export 
+
+// module.exports.sum = (a, b) => a + b;
+
+// exports.mul = (a, b) => a * b;
+
+// module.exports=5;
+
+// module.exports={
+//     a:10,
+//     b:20,
+//     c:30
+// }
+
+
+
+// module.exports="manoj";
+
+let obj={
+    name:"manoj",
+    age:20,
+    rollNO:10
+}
+
+module.exports=obj;
---
+++ Node.js/runMathUsingModule.Exports.js
@@ -0,0 +1,6 @@
+const math=require("./math")
+
+// console.log(math.name);
+// console.log(math.a);
+
+console.log(math.name);
