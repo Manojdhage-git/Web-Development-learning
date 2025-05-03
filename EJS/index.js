444e5abca6318ed886fe0fec9abe4732baf951d1
Manoj Dhage <manojdhage1341@gmail.com>
2025-05-03 09:52:39 +0530

    some file added

--- EJS/index.js
+++ EJS/index.js
@@ -1,19 +1,25 @@
-const express=require("express");
-const path=require("path")
-const app=express();
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const port=8080;
+const port = 8080;
 
-app.set("view engine","ejs");
-app.set('views',Path.join(__dirname,"/views"));// when server runs outside the folder then we have to set views and acquire path
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "/views")); // when server runs outside the folder then we have to set views and acquire path
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("home.ejs");//.ejs not necessary
 })
 
 
+// geting data from database -- passing data into Ejs
+app.get("/rolldice", (req, res) => {
+    let diceval=Math.floor(Math.random()*6)+1;
+res.render("rolldice.ejs",{num:diceval})
+})
+ 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening port ${port}`)
 })
 
