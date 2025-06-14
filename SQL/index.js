7850c9c5ab8c2c1b853d5669743ec1edb8b7b71c
Manoj Dhage <manojdhage1341@gmail.com>
2025-06-14 09:29:27 +0530

    sql started

---
+++ SQL/index.js
@@ -0,0 +1,16 @@
+const { faker } = require('@faker-js/faker');
+
+//generate random data using faker
+let createRandomUser=() =>{
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+console.log(createRandomUser());
