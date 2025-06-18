2380009b79941599cd971c5b8d44b22a1080ff2a
Manoj Dhage <manojdhage1341@gmail.com>
2025-06-18 10:03:46 +0530

    mongo with js completed

--- MongoDB/books.js
+++ MongoDB/books.js
@@ -18,15 +18,28 @@ const bookSchema = new mongoose.Schema({
     title:{
         type:String,
         required: true,
+        maxLength:20,
     },
     author:{
         type:String,
     },
     price:{
         type:Number,
-    }
+        //error handling in validation
+        min:[1,"Price is too low for Amazon Selling"]
+    },
+    discount:{
+        type:Number,
+        default:0,
+    },
+    category:{ 
+        type:String,
+        enum:["fiction","non-fiction"]
+    },
+    genre:[String],
+        })
 
-})
+//Explore mongoosejs to find more contraint.....
 
 const Book = mongoose.model("Book",bookSchema);
 
@@ -34,6 +47,8 @@ let book1 = new Book({
     title :"Mathematics XII",
     author:"RD Sharma",
     price:1200,
+    category:"fiction", //if this category not defined in the schema then it will give error.....
+    genre:["abc","xyz"]
 })
 
 book1
@@ -41,5 +56,20 @@ book1
     .then((res)=>{
         console.log(res);
     }).catch((err)=>{
-        console.log(err);
+        console.log(err.errors.price.properties.message);//can access perticular errror
     });
+
+
+
+//Update-- validation---above validation works for insertion for validation of update needs----
+//needs to set true - runvalidators:true
+
+
+book.findByIdAndUpdate("id",{price:-500},{runValidators:true})//id means id of user that generrated by mongodb
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
