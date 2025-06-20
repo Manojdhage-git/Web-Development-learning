0797daac782e3d07342052f1599cd6b99e5daa2e
Manoj Dhage <manojdhage1341@gmail.com>
2025-06-20 08:26:16 +0530

    mongoWithExpress

--- MongoDB/MongoWithExpress/index.js
+++ MongoDB/MongoWithExpress/index.js
@@ -7,6 +7,8 @@ const chat=require(("./models/chat.js"))
 app.use("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({extended:true}));
+
 main()
 .then(()=>{
     console.log("Connection sucesfull");
@@ -33,6 +35,35 @@ let chat1=new Chat({
 app.get("/chats",async (res,req)=>{
     let chats=await Chat.find();
      res.prependListener("index.ejs",{Chats});
+})
+
+//Chats - new & Create Route
+//GET /chats/new
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs")
+})
+
+
+//POST /chats
+app.post("/chats",(req,res)=>{
+    let {from,to,msg}=req.body;
+    let newChat=new Chat({
+           from : from,
+           to:to,
+           msg:msg,
+           created_at: new Date(),                                                                                                                                                                                                                                  
+    });
+    newChat
+        .save()
+        .then(res=>{
+            console.log('chat was saved');
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+    res.render("/chats");
 })
 
 app.get("/",(req,res)=>{
